bb9e87a29be605596ea7c4e2c990ced57979c941
IGA26 <160011326+IGA26@users.noreply.github.com>
2025-11-04 16:24:20 +0000

    Add files via upload

--- finetune_sil_model.py
+++ finetune_sil_model.py
@@ -254,7 +254,7 @@ def train_model(
             weight_decay=0.01,
             logging_dir=f"{output_dir}/{label_type}/logs",
             logging_steps=50,
-            evaluation_strategy="epoch",
+            eval_strategy="epoch",
             save_strategy="epoch",
             load_best_model_at_end=True,
             metric_for_best_model="accuracy",
